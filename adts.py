1253ea5c163c05eb59c6d589c8095b3e6f0e284b
Field4 <lauren.eb.field@hotmail.com>
2023-02-21 17:52:33 +0000

    Initial commit

---
+++ adts.py
@@ -0,0 +1,98 @@
+# Linked lists as nodes
+class LinkedListNode:
+    def __init__(self, value):
+        self.__value = value
+        self.__nextNode = None
+
+    def insertval(self, val):
+        if self.__value == val:
+            return "already inserted"
+        elif self.__nextNode is None:
+            self.__nextNode = LinkedListNode(val)
+            return "inserted"
+        elif self.__value < val < self.__nextNode.getvalue():
+            temp = self.__nextNode
+            self.setnextnode(LinkedListNode(val))
+            self.__nextNode.setnextnode(temp)
+            return "inserted"
+        else:
+            return self.__nextNode.insertval(val)
+
+    def deletenode(self, val):
+        if self.__value > val:
+            return "Not in list"
+        elif self.__value < val and self.__nextNode.getvalue() == val:
+            self.__nextNode = self.__nextNode.getnextnode()
+        elif self.__value < val and self.__nextNode is not None:
+            return self.__nextNode.deletenode(val)
+        else:
+            return "Not in list"
+
+    def setnextnode(self, node):
+        self.__nextNode = node
+
+    def getnextnode(self):
+        return self.__nextNode
+
+    def getvalue(self):
+        return self.__value
+
+    def printlist(self):
+        print(self.__value)
+        if self.__nextNode is not None:
+            self.__nextNode.printlist()
+
+
+# Binary tree as Nodes
+class BinaryNode:
+    def __init__(self, g_val):
+        self.__val = g_val
+        self.__left = None
+        self.__right = None
+
+    def insert(self, value):
+        if value == self.__val:
+            return "already inserted"
+        elif value < self.__val:
+            if self.__left is None:
+                self.__left = BinaryNode(value)
+                return "inserted"
+            else:
+                return self.__left.insert(value)
+        else:
+            if self.__right is None:
+                self.__right = BinaryNode(value)
+                return "inserted"
+            else:
+                return self.__right.insert(value)
+
+    def search(self, value):
+        if value == self.__val:
+            return "found"
+        elif value < self.__val:
+            if self.__left is None:
+                return "not found"
+            else:
+                return self.__left.search(value)
+        else:
+            if self.__right is None:
+                return "not found"
+            else:
+                return self.__right.search(value)
+
+    def nodeprint(self):
+        if self.__left:
+            self.__left.nodeprint()
+        print(self.__val)
+        if self.__right:
+            self.__right.nodeprint()
+
+
+root = LinkedListNode(5)
+root.insertval(6)
+root.insertval(8)
+root.insertval(7)
+root.printlist()
+root.deletenode(7)
+print("7 deleted")
+root.printlist()
---
+++ searches.py
@@ -0,0 +1,28 @@
+def linear_search(array, item):
+    i = 0
+    for i in range(len(array)):
+        if array[i] == item:
+            return True
+
+
+def binary_search(array, item):
+
+    lowerbound = 0
+    upperbound = len(array)
+    while lowerbound <= upperbound:
+        index = int((lowerbound + upperbound) / 2)
+        if array[index] == item:
+            return True
+        elif array[index] < item:
+            lowerbound = index + 1
+        else:
+            upperbound = index - 1
+
+
+values = [1, 4, 67, 35, 49, 69, 84, 325, 84]
+sorted_values = [1, 2, 5, 8, 34, 36, 45, 49, 56, 78, 90]
+if linear_search(values, 49):
+    print("Linear")
+
+if binary_search(sorted_values, 49):
+    print("binary")
