358ee93cfd2176b97f1b5088eef3fe45797fc4af
Field4 <lauren.eb.field@hotmail.com>
2023-02-21 18:33:01 +0000

    cleaning

--- sorts.py
+++ sorts.py
@@ -4,19 +4,19 @@ def bubblesort(a):
     while swap:
         swap = False
         for i in range(length):
-            if a[i] > a[i+1]:
+            if a[i] > a[i + 1]:
                 temp = a[i]
-                a[i] = a[i+1]
-                a[i+1] = temp
+                a[i] = a[i + 1]
+                a[i + 1] = temp
                 swap = True
         i -= 1
 
-def insertionsort(a):
-    for i in range(1,len(a)):
-        temp = a[i]
-        j = i-1
-        while j >= 0 and a[j] > temp:
-            a[j+1] = a[j]
-            j-=1
-        a[j] = temp
 
+def insertionsort(a):
+    for i in range(1, len(a)):
+        temp = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > temp:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j] = temp
